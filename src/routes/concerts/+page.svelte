e1180c1d54c375c50b21d1a79988586b7644d947
kergann <kergann.lecornec@gmail.com>
2023-02-17 16:06:06 +0100

    cardre

--- src/routes/concerts/+page.svelte
+++ src/routes/concerts/+page.svelte
@@ -82,7 +82,7 @@
 
 <div class="flex flex-col-reverse lg:flex-row w-full pt-32">
 	<div class="lg:w-1/3 mx-10 lg:mx-auto pt-10 lg:pt-0">
-		<img class="object-cover w-full rounded-xl" src="Sans_Cadre.jpg" alt="" />
+		<img class="object-cover w-full rounded-xl" src="Sans_Cardre.jpg" alt="" />
 	</div>
 	<ol class="lg:w-1/2 pl-10 lg:pl-0">
 		{#each concerts as concert}
