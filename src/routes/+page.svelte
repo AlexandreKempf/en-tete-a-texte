af58be8e6d4ee71c6fc2ca060a9ee0429578102b
Aleexo <alexandre@lixo.tech>
2023-02-19 19:16:25 +0100

    fix acceuil

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,50 @@
 <script>
 	import { pageTitle } from './stores.js';
+	import { fly } from 'svelte/transition';
 
 	$pageTitle = 'acceuil';
 </script>
 
-<div class="py-32 lg:py-48 2xl:w-1/2 lg:w-2/3 w-full mx-auto">
-	<img class="rounded-xl mx-auto" src="acceuil.jpg" alt="picture on stage" />
-	<h1 class="my-14 font-extrabold text-5xl text-center">En tête à texte</h1>
-	<p class="mx-5 lg:mx-0 my-8 font-light">
-		En tête à texte est un duo de chanson française, créé début 2021 par deux frères adeptes de
-		poésie et de musique.
-	</p>
-	<p class="mx-5 lg:mx-0 my-8 font-light">
-		Le but du projet ? Faire passer des messages à travers le rire, la colère, la tristesse tout en
-		partageant le goût du texte au public. Selon eux, l'art est une approche sensible qui, en
-		faisant rêver, doit questionner, contester et idéalement fédérer. De facto, de grands concepts
-		tels l'amour, le temps, l'absurdité humaine sont des thématiques récurrentes au sein de leurs
-		compositions et de leurs influences (Brassens, Rostand, La Fontaine).
-	</p>
-	<p class="mx-5 lg:mx-0 my-8 font-light">
-		La poésie se suffisant souvent à elle-même, les moyens de l'habiller sont nombreux, et
-		l'éclectisme est de mise. Une vingtaine de compositions pour quatre-vingt-dix minutes de
-		spectacle, au cours duquel des fables théâtralisées se glissent, les instruments s'échangent et
-		les rythmes et harmonies surprennent. Tous deux pianistes, guitaristes et chanteurs, ils ont su
-		créer un univers musical riche (intervention de beatbox, guimbarde, mélodica) aux influences
-		manouches et folk.
-	</p>
-	<p class="mx-5 lg:mx-0 my-8 font-light">
-		L'année 2022 a été clef pour le groupe, avec la création de leur premier album, de quatre
-		livrets illustrés et du spectacle qui a tourné en Bretagne, dans les bars (notamment du
-		finistère sud et de belle-île-en-mer), les festivals (Plein' R, les Tribus en Folies, fête de la
-		musique) et dans la rue.
-	</p>
-	<p class="mx-5 lg:mx-0 my-8 font-light">
-		À présent, le groupe se tourne également vers des lieux plus intimistes (café-librairie,
-		médiathèque, concert chez l'habitant, festival de conte et de chanson française) à la recherche
-		d'une écoute attentionnée.
-	</p>
-	<p class="mx-5 lg:mx-0 my-8 text-center font-bold">Venez les découvrir !</p>
+<div class="pt-32">
+	<div class="w-10/12 xl:w-6/12 mt-10 mx-10">
+		<h1 class="my-14 font-bold text-5xl">En tête à texte</h1>
+		<p class="mx-5 lg:mx-0 my-5 font-light text-xl">
+			En tête à texte est un duo de chanson française, créé début 2021 par deux frères adeptes de
+			poésie et de musique.
+		</p>
+		<p class="mx-5 lg:mx-0 my-5 font-light text-xl">
+			Le but du projet ? Faire passer des messages à travers le rire, la colère, la tristesse tout
+			en partageant le goût du texte au public. Selon eux, l'art est une approche sensible qui, en
+			faisant rêver, doit questionner, contester et idéalement fédérer. De facto, de grands concepts
+			tels l'amour, le temps, l'absurdité humaine sont des thématiques récurrentes au sein de leurs
+			compositions et de leurs influences (Brassens, Rostand, La Fontaine).
+		</p>
+		<p class="mx-5 lg:mx-0 my-5 font-light text-xl">
+			La poésie se suffisant souvent à elle-même, les moyens de l'habiller sont nombreux, et
+			l'éclectisme est de mise. Une vingtaine de compositions pour quatre-vingt-dix minutes de
+			spectacle, au cours duquel des fables théâtralisées se glissent, les instruments s'échangent
+			et les rythmes et harmonies surprennent. Tous deux pianistes, guitaristes et chanteurs, ils
+			ont su créer un univers musical riche (intervention de beatbox, guimbarde, mélodica) aux
+			influences manouches et folk.
+		</p>
+		<p class="mx-5 lg:mx-0 my-5 font-light text-xl">
+			L'année 2022 a été clef pour le groupe, avec la création de leur premier album, de quatre
+			livrets illustrés et du spectacle qui a tourné en Bretagne, dans les bars (notamment du
+			finistère sud et de belle-île-en-mer), les festivals (Plein' R, les Tribus en Folies, fête de
+			la musique) et dans la rue.
+		</p>
+		<p class="mx-5 lg:mx-0 my-5 font-light text-xl">
+			À présent, le groupe se tourne également vers des lieux plus intimistes (café-librairie,
+			médiathèque, concert chez l'habitant, festival de conte et de chanson française) à la
+			recherche d'une écoute attentionnée.
+		</p>
+		<a href="concerts">
+			<p class="mx-5 lg:mx-0 mt-5 text-2xl font-bold">Venez les découvrir !</p>
+		</a>
+	</div>
+	<img
+		class="xl:fixed xl:right-0 xl:bottom-0 w-full xl:w-5/12 mx-auto object-contain"
+		src="test_acceuil.jpg"
+		alt="the duo on stage"
+	/>
 </div>
--- src/routes/paroles/+page.svelte
+++ src/routes/paroles/+page.svelte
@@ -5,5 +5,5 @@
 </script>
 
 <div class="pt-40">
-	<object class="mx-auto w-full lg:w-5/6" data="paroles.pdf" style="height: 80vh" />
+	<object class="mx-auto w-full lg:w-5/6" data="paroles.pdf" style="height: 120vh" />
 </div>
