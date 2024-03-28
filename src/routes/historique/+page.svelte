92f178370bd8ccdf9fdb5c1f2593006ae4706efa
kergann <kergann.lecornec@gmail.com>
2024-03-28 11:42:05 +0100

    Update Historique

--- src/routes/historique/+page.svelte
+++ src/routes/historique/+page.svelte
@@ -5,6 +5,157 @@
 	// date start at 0 for months
 	var concerts = [
 		
+		
+		
+		{	
+			date: new Date(2023, 1, 24, 20, 0),
+			place: "Café librairie la terrible, Camaret"
+		},
+		{
+			date: new Date(2023, 2, 1, 19, 30),
+			place: "Beaj Kafe, Brest"
+		},
+		{
+			date: new Date(2023, 2, 4, 19, 0),
+			place: "Instant T, Le Pouldu"
+		},
+		{
+			date: new Date(2023, 2, 17, 20, 0),
+			place: "Brasserie de L'imprimerie, Bannalec"
+		},
+		{
+			date: new Date(2023, 3, 8, 19, 30),
+			place: 'Chez Cousin, Nizon'
+		},
+		{
+			date: new Date(2023, 3, 28, 19, 30),
+			place: 'Annivercirque, Quimperlé'
+		},
+		{
+			date: new Date(2023, 4, 6, 19, 30),
+			place: 'Brizeux, Quimperlé'
+			
+		},
+		{
+			date: new Date(2023, 4, 12, 22, 0),
+			place: "Soirée à la grande salle Yhuel, Arzano"
+		},
+		{
+			date: new Date(2023, 4, 13, 19, 0),
+			place: 'Bugul Noz Festival, Arzano'
+		},
+		{
+			date: new Date(2023, 4, 14, 19, 0),
+			place: "Fête de la Tonte, Bannalec"
+		},
+		{
+		date: new Date(2023, 4, 26, 19, 0),
+			place: "La Cave, Moëlan Sur Mer"
+		},
+		{
+			date: new Date(2023, 4, 27, 19, 0),
+			place: "Castel Reuz, Bannalec"
+		},
+		{
+			date: new Date(2023, 4, 28, 19, 0),
+			place: "Festival Coeur de Conte, Plouray"
+		},
+		{
+			date: new Date(2023, 5, 3, 19, 0),
+			place: "La Chapelle, Locminé"
+		},
+		{
+			date: new Date(2023, 5, 17, 19, 0),
+			place: "Boeuf de L'Elle, Arzano"
+		},
+		{
+			date: new Date(2023, 5, 18, 19, 0),
+			place: "Portes ouvertes des Jardins de Kermerour, Bannalec"
+		},
+		{
+			date: new Date(2023, 5, 24, 19, 0),
+			place: "Fête de la musique, Riec Sur Bélon"
+		},
+		{
+			date: new Date(2023, 6, 4, 20, 0),
+			place: "CIORTF Beg Meil, Fouenant"
+		},
+		{
+			date: new Date(2023, 6, 8, 22, 30),
+			place: "Compagnie Kaboum, Lanvénégen"
+		},
+		{
+			date: new Date(2023, 6, 13, 19, 0),
+			place: "Kistin Café, Runan"
+		},
+		{
+			date: new Date(2023, 6, 15, 19, 0),
+			place: "L'Improbable Café, Plestin Les Grèves"
+		},
+		{
+			date: new Date(2023, 6, 21, 20, 0),
+			place: "Domaine de Beg Porz, Kerfany Les Pins"
+		},
+		{
+			date: new Date(2023, 6, 22, 19, 0),
+			place: "Ferme de Keranmevel, Saint Yvi"
+		},
+		{
+			date: new Date(2023, 6, 28, 18, 0),
+			place: "A l'abri de l'océan, Baye"
+		},	
+		{
+			date: new Date(2023, 7, 4, 20, 0),
+			place: "Domaine de Beg Porz, Kerfany Les Pins"
+		},
+		{
+			date: new Date(2023, 7, 6, 18, 0),
+			place: "Salle des Fête , Plouguiel"
+		},
+		{
+			date: new Date(2023, 7, 5, 18, 0),
+			place: "Fête Village, Kerbors"
+		},
+		{
+			date: new Date(2023, 7, 11, 19, 0),
+			place: "Le Bar Bichette, Trégunc"
+		},
+		{
+			date: new Date(2023, 7, 12, 19, 0),
+			place: "Le Melar Dit, Locmélar"
+		},
+		{
+			date: new Date(2023, 7, 19, 16, 0),
+			place: 'Nuit des love shacks, Nizon'
+		},
+		{
+			date: new Date(2023, 7, 20, 22, 30),
+			place: "Compagnie Kaboum, Lanvénégen"
+		},
+		{
+			date: new Date(2023, 7, 25, 20, 0),
+			place: "Domaine de Beg Porz, Kerfany Les Pins"
+		},
+		{
+			date: new Date(2023, 7, 26, 19, 0),
+			place: "Disjonct'Fest, Clohars Carnoet"
+		},
+		{
+			date: new Date(2023, 7, 25, 14, 0),
+			place: "Festival Voulez-Vous Vivre ?, Névez"
+		},
+		{
+			date: new Date(2023, 7, 27, 16, 0),
+			place: "Ferme au 10 vaches, Querrien"
+		},
+		{
+			date: new Date(2023, 7, 30, 19, 0),
+			place: "CIORTF, Beg Meil"
+		},
+		{
+			date: new Date(2023, 10, 11 , 20, 0),
+			place: "Le Ptit Bar Contrevent, Tréguidel"
+		},
 		{
 			date: new Date(2023, 11, 1 , 20, 0),
 			place: " La Guinguette de Croas An Ter , Clohars"
@@ -46,6 +197,11 @@
 			
 		},
 		{
+			date: new Date(2024, 2, 15, 20, 0),
+			place: "Le Relais, Lanmodez"
+			
+		},
+		{
 			date: new Date(2024,3,5,18),
 			place: "Festival Chanson de Café, Pornic"
 		},
@@ -76,7 +232,12 @@
 		{
 			date: new Date(2024,6,13,18),
 			place: "Les Jeunes Binettes, Trémargat"
+		},
+		{
+			date: new Date(2024,7,14,18),
+			place: "Jardin, Moelan-Sur-Mer"
 		}
+
 
 
 
