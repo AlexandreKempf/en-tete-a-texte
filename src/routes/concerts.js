cd7b18932465ebb46259383fe724bf0e6179196f
kergann <kergann.lecornec@gmail.com>
2024-07-05 17:40:49 +0200

    Update concerts.js

--- src/routes/concerts.js
+++ src/routes/concerts.js
@@ -255,7 +255,7 @@ export const concerts = [
     },
     {
         date: new Date(2024, 6, 11, 19),
-        place: 'Les jeudis de Kermouster, Lezardrieux'
+        place: 'La Cambuse De Kermouster, Lezardrieux'
     },
     {
         date: new Date(2024, 6, 12, 20),
