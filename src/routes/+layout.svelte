b2a4b673196617ccbb69c6ca89f414f0492e2c1a
AlexandreKempf <alexandre.kempf@cri-paris.org>
2025-08-08 10:49:08 +0200

    change name of the website

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,7 +21,7 @@
 	}
 </script>
 
-<title>En-Tête-en-Texte</title>
+<title>En-Tête-a-Texte</title>
 <div class="absolute flex w-full" bind:clientWidth={screenSize}>
 	<div class="inline-flex w-1/2 flex-col">
 		{#if screenSize >= 1550}
